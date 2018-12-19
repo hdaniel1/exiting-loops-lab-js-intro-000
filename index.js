bc28af175992fe4dcaec2355472606c8933600f7
Daniel Hallada <hallada.daniel@gmail.com>
2018-12-19 19:57:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function breakOut(array, changeValue, stopValue) {
   }
   array[i] = changeValue
 } return array}
+
+function keepGoing(array, changeValue, skipValue) {
+  for (let i = 0; i < array.length;i++) {
+    if (array[i] === skipValue) {
+      continue
+    } 
+    array[i] = changeValue
+  }
+  return array
+}
