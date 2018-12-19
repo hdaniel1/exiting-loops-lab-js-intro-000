2d33e9465d508df8431a441c1551721a93956ae5
Daniel Hallada <hallada.daniel@gmail.com>
2018-12-19 19:44:14 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function breakOut(array, changeValue, stopValue) {
+  for (let i = i; i < length.array; i++) {
+    if (array[i] === stopValue) {
+      break
+  }
+  array[i] = changeValue
+} return array}
