9b9f5450be87ab7ca179b1ecdb41c5e1e4a2f2c5
Daniel Hallada <hallada.daniel@gmail.com>
2018-12-19 19:57:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function keepGoing(array, changeValue, skipValue) {
   for (let i = 0; i < array.length;i++) {
     if (array[i] === skipValue) {
       continue
-    } 
+    }
     array[i] = changeValue
   }
   return array
